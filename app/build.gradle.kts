5431569b2cfd2c52b30bdbd7673df9ac9b04d701
pol <chepemingo7@otmail.com>
2023-10-16 10:23:35 -0600

    Hola

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     /*Dependencias para CameraX */
-
+    implementation ("androidx.camera:camera-core:1.2.3")
     implementation ("androidx.camera:camera-camera2:1.0.0")
     implementation ("androidx.camera:camera-lifecycle:1.0.0")
     implementation ("androidx.camera:camera-view:1.0.0-alpha25")
-
 }
